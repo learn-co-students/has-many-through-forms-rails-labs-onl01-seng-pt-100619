be69c3a77843192e4f2998de9a7ca930d97518b6
pjavadi84 <pouya.javadi.resume@gmail.com>
2020-03-21 14:17:22 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
+
+  def comment_attributes=(comment_user_attributes)
+    comment_user_attributes.values.each do |user_attributes_username|
+      comment = Comment.find_or_create_by(user_attributes_username)
+        self.comments << comment
+    end
+  end
 
 end
