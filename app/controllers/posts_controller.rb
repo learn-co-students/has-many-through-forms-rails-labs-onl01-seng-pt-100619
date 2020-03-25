bd80b3daf21983f51adb319f232fe66a2b83d547
steelerx155 <steelerx155@gmail.com>
2020-03-25 16:59:03 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,3 +22,17 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
   end
 end
+
+
+
+# <%= render 'form', post: Post.new %>
+# <%= form_for @post do |f| %>
+#     <%= f.label "Title" %>
+#     <%= f.text_field :title %>
+#     <%= f.label "Content" %>
+#     <%= f.text_field :content %>
+#     <%= f.collection_check_boxes :category_ids, Category.all, :id, :name %>
+#     <%- f.submit %>
+#     <% end %>
+    
+#     <%= link_to post.title, post_path(@post.id) %>
