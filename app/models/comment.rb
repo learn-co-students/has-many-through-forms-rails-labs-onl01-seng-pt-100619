fea2b7a35b405eba1257df98fa9e4baf58b0ef93
justin <jayman1294@gmail.com>
2020-03-25 14:34:56 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,20 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :reject_user
 
+  
+  def comments_attributes=(comment_attributes)
+    comment_attributes.values.each do |comment_attribute|
+      comment = Comment.find_or_create_by(comment_attribute)
+      self.comments << comment
+    end
+  end
+
+
+  
+
+  def reject_user(attributes)
+    attributes['username'].blank?
+  end
 end
