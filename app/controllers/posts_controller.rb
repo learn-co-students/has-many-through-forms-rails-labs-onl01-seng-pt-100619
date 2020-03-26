53547d57fd06a34a4feae3856444ed38b6a32564
steelerx155 <steelerx155@gmail.com>
2020-03-25 22:13:58 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.create(post_params)
+    post = Post.find_or_create_by(post_params)
     redirect_to post
   end
 
